6bd6bb3e60a8145df40c2f10c46de41ed0569a9b
Shelvin Kumar <108801242+ShelvinKumar@users.noreply.github.com>
2023-08-09 14:33:31 +1200

    Vercel
    Working on the vercel URL

--- frontend-week3/app.js
+++ frontend-week3/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./Config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
